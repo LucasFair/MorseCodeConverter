c28677c479b00ac9c78f63840d498225fddc234e
LucasFair <141710445+LucasFair@users.noreply.github.com>
2023-08-11 21:21:52 +0200

    Adding files to repo.

---
+++ MorseCodeConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MorseCodeConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // — ·
+
+            Dictionary<string, string> morseDict = new Dictionary<string, string>()
+            {
+                {"A" ,"· —"}, {"B", "— · · ·"}, {"C", "— · — ·"}, {"D", "— · ·"}, {"E", "·"}, {"F", "· · — ·"},
+                {"G", "— — ·"}, {"H", "· · · ·"}, {"I", "· ·"}, {"J", "· — — —"}, {"K", "— · —"}, {"L", "· — · ·"},
+                {"N", "— ·"}, {"M", "— —"}, {"O", "— — —"}, {"P", "· — — ·"}, {"Q", "— — · —"}, {"R", "· — ·"},
+                {"S", "· · ·"}, {"T", "—"}, {"U", "· · —"}, {"V", "· · · —"}, {"W", "· — —"}, {"X", "— · · —"},
+                {"Y", "— · — —"}, {"Z", "— — · ·"} 
+
+            };
+
+
+            foreach (KeyValuePair<string, string> elmnt in morseDict)
+            {
+                Console.WriteLine("{0} is {1}", elmnt.Key, elmnt.Value);
+            }
+        }
+    }
+}
